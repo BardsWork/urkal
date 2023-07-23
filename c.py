79e43f7bfe1f85cce69d2ca34ede155b1a0f4d80
Alex Bard <alex@bardworx.com>
2023-07-22 22:17:04 -0400

    added font

--- c.py
+++ c.py
@@ -2,30 +2,31 @@ from __future__ import print_function
 
 from PIL import Image, ImageDraw, ImageFont
 
-from lib.waveshare import epd7in5_V2
+# from lib.waveshare import epd7in5_V2
 from lib.gcal.main import get_events
 
 import datetime
 from datetime import date
 import calendar
 
+EPD_WIDTH = 800
+EPD_HEIGHT = 480
+
 
 def main():
-    events = get_events()
-
     # E-paper part
-    epd = epd7in5_V2.EPD()
-    epd.init()
-    epd.Clear()
+    # epd = epd7in5_V2.EPD()
+    # epd.init()
+    # epd.Clear()
 
-    font12 = ImageFont.truetype("./fonts/Font.ttc", 12)
-    font14 = ImageFont.truetype("./fonts/Font.ttc", 14)
-    font15 = ImageFont.truetype("./fonts/Font.ttc", 15)
-    font18 = ImageFont.truetype("./fonts/Font.ttc", 18)
-    font24 = ImageFont.truetype("./fonts/Font.ttc", 30)
-    font56 = ImageFont.truetype("./fonts/Font.ttc", 72)
+    font12 = ImageFont.truetype("./fonts/font.ttc", 12)
+    font14 = ImageFont.truetype("./fonts/font.ttc", 14)
+    font15 = ImageFont.truetype("./fonts/font.ttc", 15)
+    font18 = ImageFont.truetype("./fonts/font.ttc", 18)
+    font24 = ImageFont.truetype("./fonts/font.ttc", 30)
+    font56 = ImageFont.truetype("./fonts/font.ttc", 72)
 
-    Himage = Image.new('1', (epd.width, epd.height), 255)
+    Himage = Image.new('1', (EPD_WIDTH, EPD_HEIGHT), 255)
     draw = ImageDraw.Draw(Himage)
 
     # TODAY
@@ -34,72 +35,73 @@ def main():
     draw.text((110, 60), date.today().strftime("%A"), font=font24, fill=0)
 
     # CALENDAR SETUP
-    today = datetime.date.today()
-    calendar.setfirstweekday(6)
-    calendar_days = calendar.monthcalendar(today.year, 3)
+    events = get_events()  # retrieves events from google calendar API
+    today = datetime.date.today()  # needed to filter elements
+    calendar.setfirstweekday(6)  # formats the calendar to start on sunday
+    calendar_days = calendar.monthcalendar(today.year, today.month)  # provides current year and month
+    days_of_week = calendar.day_abbr[-1:] + calendar.day_abbr[:-1]  # calendar starts on Monday but we want Sunday
 
     # POSITIONING OF FIRST ELEMENT
-    left = 0
-    top = 150
+    calendar_y = 0
+    calendar_x = 150
+    calendar_offset = 30
 
-    days_of_week = calendar.day_abbr[-1:] + calendar.day_abbr[:-1]
-    # PRINT DAYS
+    # PRINT DAYS OF THE WEEK
     for day in days_of_week:
-        left += 30
-        draw.text((left, top), day[:2], font=font18)
+        calendar_y += calendar_offset
+        draw.text((calendar_y, calendar_x), day[:2], font=font18)
 
-    # PRINT CALENDAR MONTH
+    # PRINT CALENDAR MONTH DAYS IN CALENDAR FORMAT
     for row in calendar_days:
-        left = 0
-        top += 30
+        calendar_y = 0  # resets starting position
+        calendar_x += calendar_offset
+
         for day in row:
-            left += 30
-            if day > 0:
-                draw.text((left, top), str(day), font=font18)
+            calendar_y += calendar_offset
+            if day > 0:  # zero means no date to draw
+                draw.text((calendar_y, calendar_x), str(day), font=font18)
 
     # Loop trough calendar events and draw them to buffer
     x = 400  # start position on x-axis of events on e-ink screen
     y = 30  # start position on y-axis of events on e-ink screen
-    curweek = ''  # variable for week of event
-    curday = ''  # variable for day of event
+    current_week = ''  # variable for week of event
+    current_day = ''  # variable for day of event
 
     for event in events:
-
-        start = event['start']
-        start_datetime = start['dateTime']
-
         start = event['start'].get('dateTime', event['start'].get('date'))
         end = event['end'].get('dateTime', event['end'].get('date'))
-        summ = event['summary']
+        summary = event['summary']
+        start_date = None
+        end_date = None
 
         if len(start) == 10:  # events that are 'whole day'-events
-            startdate = datetime.datetime.strptime(start, "%Y-%m-%d")
-            enddate = datetime.datetime.strptime(end, "%Y-%m-%d")
+            start_date = datetime.datetime.strptime(start, "%Y-%m-%d")
+            end_date = datetime.datetime.strptime(end, "%Y-%m-%d")
             time = ''
         if len(start) == 25:  # events that start at specific time
-            startdate = datetime.datetime.strptime(start, "%Y-%m-%dT%H:%M:%S%z")
-            enddate = datetime.datetime.strptime(end, "%Y-%m-%dT%H:%M:%S%z")
-            time = startdate.strftime(" (%H:%M -")
-            fin_time = enddate.strftime(" %H:%M) ")
+            start_date = datetime.datetime.strptime(start, "%Y-%m-%dT%H:%M:%S%z")
+            end_date = datetime.datetime.strptime(end, "%Y-%m-%dT%H:%M:%S%z")
+            time = start_date.strftime(" (%H:%M -")
+            fin_time = end_date.strftime(" %H:%M) ")
 
-            start_offset = abs(8 - int(startdate.strftime("%H")))
-            end_offset = abs(8 - int(enddate.strftime("%H")))
+            start_offset = abs(8 - int(start_date.strftime("%H")))
+            end_offset = abs(8 - int(end_date.strftime("%H")))
             x1 = 380
             x2 = 540
-            y1 = 102 + (start_offset * 60) + int(startdate.strftime("%M"))
-            y2 = 98 + (end_offset * 60) + int(enddate.strftime("%M"))
+            y1 = 102 + (start_offset * 60) + int(start_date.strftime("%M"))
+            y2 = 98 + (end_offset * 60) + int(end_date.strftime("%M"))
             draw.rounded_rectangle((x1, y1, x2, y2), radius=4, fill=None, outline=None, width=1)
-            draw.text((x1, y1), event['summary'])
+            draw.text((x1, y1), summary)
 
-        startdate_date = startdate.date()
+        startdate_date = start_date.date()
         if startdate_date < date.today():
-            startdate = date.today()
+            start_date = date.today()
             startdate_date = date.today()
-        day = startdate.strftime("%a %m-%d")
-        week = startdate.strftime("%V")
+        day = start_date.strftime("%a %m-%d")
+        week = start_date.strftime("%V")
 
-        if curweek != week and startdate_date != date.today():
-            if (week == date.today().strftime("%V")):
+        if current_week != week and startdate_date != date.today():
+            if week == date.today().strftime("%V"):
                 x = 550
                 y = 30
                 draw.text((x, y), 'LATER THIS WEEK', font=font18, fill=0)
@@ -109,12 +111,12 @@ def main():
             #    draw.text((x, y), 'NEXT WEEK', font=font18, fill=0)
             else:
                 break
-            curweek = week
+            current_week = week
             y = y + 25
 
-        if curday != day and startdate_date != date.today():
+        if current_day != day and startdate_date != date.today():
             draw.text((x, y + 4), day.upper(), font=font18, fill=0)
-            curday = day
+            current_day = day
             y = y + 20
 
         draw.text((x, y), ' ' + event['summary'] + time + fin_time, font=font15, fill=0)
@@ -145,7 +147,8 @@ def main():
     draw.line((360, 400, 760, 400), width=1)
 
     # Display buffer on the screen
-    epd.display(epd.getbuffer(Himage))
+    # epd.display(epd.getbuffer(Himage))
+    Himage.show()
 
 
 if __name__ == '__main__':
